bf338dbd91440ad410c222ebd5bcaeb5420f04a2
carlosmdarribas <carlosmdarribas@usal.es>
2021-01-15 14:35:22 +0100

    Finalizada sección principal, a falta de dividir por sección lógicamente Añadida interfaz detail, pendiente de conectar

---
+++ La Cervecería de Pepe/Libraries/WebRequests.swift
@@ -0,0 +1,8 @@
+//
+//  WebRequests.swift
+//  La Cervecería de Pepe
+//
+//  Created by Carlos on 15/01/2021.
+//
+
+import Foundation
---
+++ La Cervecería de Pepe/VCs/ProductViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProductViewController.swift
+//  La Cervecería de Pepe
+//
+//  Created by Carlos on 15/01/2021.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
